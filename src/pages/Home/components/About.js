5cab61996df59c1d374f9fc9f2c3a5b47d9eb16b
Md: Mohon <mdmohon6145@gmail.com>
2023-03-23 01:20:59 +0600

    navbar added

---
+++ src/pages/Home/components/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1 className="text-7xl font-bold w-[80%] mx-auto text-center text-[#330523] my-8">
+        Become digitally better with our continuous delivery improvement model
+      </h1>
+      <div className="w-[400px] mx-auto flex">
+        <div className="bg-[#00FFFF] text-xl w-[200px] text-center py-2">
+          <h3>Find out more</h3>
+        </div>
+        <h3 className=" text-xl">Book a discovery call</h3>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import About from "./components/About";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>This is Home Page</h1>
+      <About />
+    </div>
+  );
+};
+
+export default Home;
